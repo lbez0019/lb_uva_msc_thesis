4a0e8bcb216d502f384697c8ca73ab48903eb717
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2023-04-16 16:00:22 +0200

    Complete XML (UBL) Parsing

--- sampleUblHandler.py
+++ sampleUblHandler.py
@@ -4,18 +4,18 @@ import os
 
 class UBLHandler(xml.sax.ContentHandler):
     def __init__(self):
+        self.document_name = ""
         self.current_element = ""
-        self.facts = []
-        self.fact_values = []
+        self.basic_element_add_flag = False
         self.aggregate_component_stack = []
-        self.basic_component_stack = []
-        self.basic_component_stack_flag = False
+        self.fact_values = []
         self.parsed_aggregate_components = []
-        # self.invoice_number = ""
-        # self.invoice_date = ""
+        self.facts = set()
 
     def startElement(self, name, attrs):
         self.current_element = name
+        # Resetting parsed CAC elements, multiple instances of same CAC are possible
+        self.parsed_aggregate_components = []
 
     def endElement(self, name):
         if name.startswith("cac"):
@@ -24,59 +24,63 @@ class UBLHandler(xml.sax.ContentHandler):
             self.current_element = ""
 
     def characters(self, content):
-        if self.current_element.startswith("cac") and self.current_element not in self.parsed_aggregate_components:
-            aggregate_value = self.current_element.split(":")[1]
-            if self.aggregate_component_stack and self.aggregate_component_stack[-1] == aggregate_value\
-                    and self.basic_component_stack:
-                self.basic_component_stack = []
-                self.basic_component_stack_flag = False
+        # First Element in UBL == Document Name
+        if self.document_name == "":
+            self.document_name = self.current_element
+
+        elif self.current_element.startswith("cac") and self.current_element not in self.parsed_aggregate_components:
+            value = self.current_element.split(":")[1]
+
+            if self.aggregate_component_stack and self.aggregate_component_stack[-1] == value \
+                    and self.basic_element_add_flag:
                 self.parsed_aggregate_components.append(self.current_element)
                 self.aggregate_component_stack.pop()
-            elif not self.basic_component_stack_flag:
-                self.basic_component_stack_flag = True
-                self.aggregate_component_stack.append(aggregate_value)
+
+            elif not self.aggregate_component_stack or self.aggregate_component_stack[-1] != value:
+                self.basic_element_add_flag = False
+                self.aggregate_component_stack.append(value)
 
         elif self.current_element.startswith("cbc"):
-            basic_component_name = self.current_element.split(":")[1]
-            if self.basic_component_stack_flag is True:
-                self.basic_component_stack.append(basic_component_name)
+            value = self.current_element.split(":")[1]
+
+            self.basic_element_add_flag = True
             fact_value = content
             fact_name = ''
 
-            for name in reversed(self.aggregate_component_stack):
+            for name in self.aggregate_component_stack:
                 fact_name += (name + '_')
+            fact_name += value
 
-            fact_name += basic_component_name
-
-            if (fact_value.isdigit() or (fact_value.startswith('-') and fact_value[1:].isdigit())):
-                self.facts.append(f'Fact {fact_name} Identified by Int')
+            if fact_value.isdigit() or (fact_value.startswith('-') and fact_value[1:].isdigit()):
+                self.facts.add(f'Fact {fact_name} Identified by Int')
             else:
-                self.facts.append(f'Fact {fact_name} Identified by String')
+                self.facts.add(f'Fact {fact_name} Identified by String')
 
             self.fact_values.append(f'+{fact_name}({fact_value})')
 
-        # if self.current_element == "cbc:ID":
-        #     self.invoice_number = content
-        # elif self.current_element == "cbc:IssueDate":
-        #     self.invoice_date = content
 
-def try_parse(parser=None):
+def try_parse(file):
     # Construct the path to the XML file
-    subdirectory = "files"
-    filepathOrder = os.path.join(subdirectory, "example-order.xml")
-    filepathInvoice = os.path.join(subdirectory, "example-invoice.xml")
-    filepathTest= "example-test.xml"
+    parser = xml.sax.make_parser()
+    document_handler = UBLHandler()
+    parser.setContentHandler(document_handler)
 
     try:
-        parser.parse(filepathTest)
-        parser.parse(filepathOrder)
-        parser.parse(filepathInvoice)
+        parser.parse(file)
+
+        print(document_handler.document_name)
+        print(document_handler.facts)
+        print(document_handler.fact_values)
+
+        # eflint_communicate(document_handler.document_name, document_handler.facts, document_handler.fact_values)
+
     except Exception as ex:
         print(ex)
     else:
         print("OK")
 
-def eflint_communicate():
+
+def eflint_communicate(document, eflint_facts, eflint_fact_values):
     # # Run WSL and move up one directory
     # result = subprocess.run(['wsl', 'cd ..', 'ls'], shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     # out, err = result.communicate()
@@ -87,12 +91,10 @@ def eflint_communicate():
     print(result.stdout.decode())
 
 
+subdirectory = "documents"
+for filename in os.listdir(subdirectory):
+    f = os.path.join(subdirectory, filename)
+    if os.path.isfile(f):
+        try_parse(f)
 
-parser = xml.sax.make_parser()
-handler = UBLHandler()
-parser.setContentHandler(handler)
-try_parse(parser)
-# eflint_communicate()
 
-# print(handler.invoice_number)
-# print(handler.invoice_date)
