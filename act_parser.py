329ccec98fcc4672eaff6ced5c3af8dd1dedc316
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2023-06-06 11:45:32 +0200

    Improvements to action parser, topological sort samples

--- act_parser.py
+++ act_parser.py
@@ -1,55 +1,81 @@
 import re
 import itertools
 from collections import defaultdict
-from pyparsing import infixNotation, opAssoc, Literal, Word, alphas
-from pyparsing import Keyword, nums, alphanums
+from pyparsing import infixNotation, opAssoc, Literal, Word, alphas, Forward
+from pyparsing import Keyword, nums, alphanums, Group, Suppress
 
-operands = set()
+class ActionParser():
+    def __init__(self):
+        self.operands = set()
 
-AND = Literal("&&")
-OR = Literal("||")
-NOT = Literal("!")
+        AND = Literal("&&")
+        OR = Literal("||")
+        NOT = Keyword("Not", caseless=True) | Literal("!")
 
-operand = Word(alphas, alphanums+"_")
-integer = Word(nums).setParseAction(lambda t: int(t[0]))
-comparison_op = Literal("==") | Literal("!=") | Literal("<=") | Literal(">=") | Literal("<") | Literal(">")
-comparison_expr = operand + comparison_op + (integer | operand)
-expr = infixNotation(comparison_expr | operand,
-    [
-        (NOT, 1, opAssoc.RIGHT),
-        (AND, 2, opAssoc.LEFT),
-        (OR, 2, opAssoc.LEFT)
-    ]
-)
+        operand = Word(alphas, alphanums+"_")
+        integer = Word(nums).setParseAction(lambda t: int(t[0]))
+        comparison_op = Literal("==") | Literal("!=") | Literal("<=") | Literal(">=") | Literal("<") | Literal(">")
+        comparison_expr = operand + comparison_op + (integer | operand)
+        self.expr = Forward()
+
+        function_call = Group(operand + Suppress("(") + Suppress(")"))  # Updated function call pattern
+        atom = comparison_expr | function_call | operand
+        self.expr <<= infixNotation(atom,
+            [
+                (NOT, 1, opAssoc.RIGHT),
+                (AND, 2, opAssoc.LEFT),
+                (OR, 2, opAssoc.LEFT)
+            ]
+        )
+
+    def parse_string(self, input_string):
+        lines = input_string.split("\n")
+        result = {'Act': [], 'Actor': [], 'Recipient': [], 'Holds when': [], 'Conditioned by': [], 'Creates': [],
+                  'Terminates': [], 'Obfuscates': []}
+        current_key = ''
+
+        for line in lines:
+            line = line.strip()
+            if line:
+                for key in result.keys():
+                    if line.startswith(key + ' '):
+                        value = line[len(key) + 1:].strip()
+
+                        if key == 'Holds when' or key == 'Conditioned by':
+                            value = self.expr.parseString(value).as_list()
+                        else:
+                            value = re.split(r"&&(?![^()]*\))", value)
+                            value = [element.strip() for element in value]
+
+                        result[key].extend(value)
+                        current_key = key
+                        break
+                else:
+                    result[current_key][-1] += ' ' + line.strip()
+
+        return result
 
 
-def calculate_permutations(lst):
-    permutations = list(itertools.product([True, False], repeat=len(lst)))
+
+
+def calculate_permutations(operands):
+    permutations = list(itertools.product([True, False], repeat=len(operands)))
     return permutations
 
 
-def parse_string(input_string):
-    lines = input_string.split("\n")
-    result = {'Act': [], 'Actor': [], 'Recipient': [], 'Holds when': [], 'Conditioned by': [], 'Creates': [],
-              'Terminates': [], 'Obfuscates': []}
-    current_key = ''
+# Function to count the number of operands in the parsed expression
+def count_operand_instances(action_parser, expression):
+    if isinstance(expression, int) or expression in ['&&', '||', '!', '==', 'Not']:
+        return 0
 
-    for line in lines:
-        line = line.strip()
-        if line:
-            for key in result.keys():
-                if line.startswith(key + ' '):
-                    value = line[len(key) + 1:].strip()
-                    split_text = re.split(r"&&(?![^()]*\))", value)
-                    value = [element.strip() for element in split_text]
-                    result[key].extend(value)
-                    current_key = key
-                    break
-            else:
-                result[current_key][-1] += ' ' + line.strip()
+    if isinstance(expression, str):
+        action_parser.operands.add(expression)
+        return 1
 
-    return result
-
+    count = 0
+    for item in expression:
+        count += count_operand_instances(action_parser, item)
+    return count
 
 def parse_file(file_path):
     with open(file_path, 'r') as file:
@@ -64,9 +90,13 @@ def parse_file(file_path):
 template_path = '../../eflint-server/web-server/test.eflint'  # Replace with the actual file path
 parsed_acts = parse_file(template_path)
 action_list = []
+action_parser = ActionParser()
+
+logic_expression_string = "Not(administrator()) || candidate == 5 && test_fact"
+parsed_expression = action_parser.expr.parseString(logic_expression_string)
 
 for act in parsed_acts:
-    action = parse_string(act)
+    action = action_parser.parse_string(act)
     action_list.append(action)
 
     for key in action:
@@ -78,50 +108,47 @@ for act in parsed_acts:
 
     print(preconditions)
 
+    operand_count = count_operand_instances(action_parser, preconditions)
     # Replace with your own list
-    permutations = calculate_permutations(preconditions)
+    permutations = calculate_permutations(action_parser.operands)
     print(len(permutations))
     for perm in permutations:
         print(perm)
 
-
-def topological_sort(dependencies):
-    graph = defaultdict(list)
-    visited = set()
-    result = []
-
-    # Build the graph
-    for item in dependencies:
-        item_name = item["Act"][0]
-        holds_when = item["Holds when"]
-        creates = item["Creates"]
-        graph[item_name].extend(creates)
-
-    # Recursive function to perform the depth-first search
-    def dfs(node):
-        visited.add(node)
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                dfs(neighbor)
-        result.insert(0, node)
-
-    # Perform topological sort for each item in the graph
-    for item_name in list(graph.keys()):  # Use a copy of keys
-        print(item_name)
-        if item_name not in visited:
-            dfs(item_name)
-
-    return result
-
-
-items = [
-    {'Act': ['declare-winner'], 'Actor': ['administrator'], 'Recipient': ['candidate'], 'Holds when': ['administrator', 'candidate', 'test-fact'], 'Conditioned by': ['Not(vote-concluded())', 'voters-done()', '(Forall other-candidate : Count(Foreach vote : vote When vote', 'vote.'], 'Creates': [], 'Terminates': [], 'Obfuscates': []},
-    {'Act': ['test-act'], 'Actor': ['tender_id'], 'Recipient': [], 'Holds when': ['tender_id, tender_id2'], 'Conditioned by': ['documents_added("tender")'], 'Creates': ['test-fact.'], 'Terminates': [], 'Obfuscates': []
-    }
-]
-
-sorted_items = topological_sort(items)
-print(sorted_items)
+print("end")
+# sorted_items = topological_sort(items)
+# print(sorted_items)
 # for item_name in sorted_items:
 #     item = next(item for item in items if item[0] == item_name)
 #     print(item)
+
+
+
+
+# def topological_sort(dependencies):
+#     graph = defaultdict(list)
+#     visited = set()
+#     result = []
+#
+#     # Build the graph
+#     for item in dependencies:
+#         item_name = item["Act"][0]
+#         holds_when = item["Holds when"]
+#         creates = item["Creates"]
+#         graph[item_name].extend(creates)
+#
+#     # Recursive function to perform the depth-first search
+#     def dfs(node):
+#         visited.add(node)
+#         for neighbor in graph[node]:
+#             if neighbor not in visited:
+#                 dfs(neighbor)
+#         result.insert(0, node)
+#
+#     # Perform topological sort for each item in the graph
+#     for item_name in list(graph.keys()):  # Use a copy of keys
+#         print(item_name)
+#         if item_name not in visited:
+#             dfs(item_name)
+#
+#     return result
--- topological_sort.py
+++ topological_sort.py
@@ -2,14 +2,17 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-
 def visualize_graph(graph):
     pos = nx.spring_layout(graph)  # Layout for node positioning
     labels = {node: f"{node}" for node in graph.nodes()}
     print(graph.edges())
-    edge_labels = {(u, v): f"{u} to {v}" for u, v in graph.edges()}
 
-    plt.figure(figsize=(10, 6))
+    edge_labels = {}
+    for u, v, attrs in graph.edges(data=True):
+        if "action" in attrs:
+            edge_labels[(u, v)] = attrs["action"]
+
+    plt.figure(figsize=(20, 12))
     nx.draw_networkx(graph, pos=pos, with_labels=True, labels=labels, node_size=800, node_color="lightblue",
                      edge_color="gray", arrows=True, font_size=12, font_weight="bold")
 
@@ -22,29 +25,29 @@ def visualize_graph(graph):
 
 def order_items(items):
     # Create a directed graph
-    init_node = 0
+    # init_node = 0
     graph = nx.DiGraph()
-    graph.add_node(init_node) # Start Node
+    # graph.add_node(init_node) # Start Node
 
     # Add nodes to the graph
     for item in items:
         values = items[item]
-        creates_nodes = values["Creates"][0]
+        creates_nodes = values["Creates"]
         for node in creates_nodes:
             graph.add_node(node)  # Convert dict to tuple
 
     # Add directed edges based on Holds and Creates clauses
     for item in items:
-        holds = items[item]["Holds"]
+        holds = items[item]["Holds when"]
         creates = items[item]["Creates"]
 
-        if not holds:
-            graph.add_edge(init_node, creates[0][0], action=item)
-            continue
+        # if not holds:
+        #     graph.add_edge(init_node, creates[0][0], action=item)
+        #     continue
 
         for hold in holds:
             for create in creates:
-                graph.add_edge(hold[0], create[0], action=item)  # Convert dict to tuple
+                graph.add_edge(hold, create, action=item)  # Convert dict to tuple
 
     visualize_graph(graph)
 
@@ -87,14 +90,36 @@ def get_action_from_edge(graph, node, successor):
             return graph.edges[node, successor]["action"]
 
 
+# items = {
+#     "A": {"Holds": [[5]], "Creates": [[1]]},
+#     "B": {"Holds": [[3], [4]], "Creates": [[2]]},
+#     # "C": {"Holds": [[1]], "Creates": [[3]]},
+#     "C": {"Holds": [[1]], "Creates": [[4]]},
+#     "D": {"Holds": [[1]], "Creates": [[3]]},
+#     "E": {"Holds": [], "Creates": [[5]]}
+# }
+
 items = {
-    "A": {"Holds": [[5]], "Creates": [[1]]},
-    "B": {"Holds": [[3], [4]], "Creates": [[2]]},
-    # "C": {"Holds": [[1]], "Creates": [[3]]},
-    "C": {"Holds": [[1]], "Creates": [[4]]},
-    "D": {"Holds": [[1]], "Creates": [[3]]},
-    "E": {"Holds": [], "Creates": [[5]]}
+    "submit_tender_documents": {  "Holds when": ['True'], "Creates": ['tender_submitted']},
+    "receive_tender_documents": { "Holds when": ['tender_submitted'], "Creates": ['tender_received']},
+    "send_awarded_notification": { "Holds when": ['tender_received'], "Creates": ['awarded_notification']},
+    "send_unawarded_notification": { "Holds when": ['tender_received'], "Creates": ['unawarded_notification']},
+    "receive_notification": { "Holds when": ['unawarded_notification', 'awarded_notification'], "Creates": ['notification_received']}
 }
+
+# items = {
+#     "submit_tender_documents": {"Holds when": ['tender_tendererparty', 'tender_contractingparty_party'], "Creates": ['tender_submitted']},
+#     "receive_tender_documents": {"Holds when": ['tender_contractingparty_party', 'tender_submitted'], "Creates": ['tender_received']},
+#     "send_awarded_notification": {"Holds when": ['awardednotification_senderparty', 'awardednotification_receiverparty', 'tender_received'],  "Creates": ['awarded_notification']},
+#     "send_unawarded_notification": {"Holds when": ['unawardednotification_senderparty', 'unawardednotification_receiverparty', 'tender_received'], "Creates": ['unawarded_notification']},
+#     "receive_notification": {"Holds when": ['unawardednotification_receiverparty', 'unawarded_notification'],  "Creates": ['notification_received']}
+# }
+
+# items = {
+#     "submit_tender_documents": {"Holds when": ['tender_tendererparty', 'tender_contractingparty_party'], "Creates": ['tender_submitted']},
+#     "receive_tender_documents": {"Holds when": ['tender_submitted'], 'Creates': ['tender_received']}
+#
+# }
 
 scenarios = order_items(items)
 for scenario in scenarios:
